0528c56a6da9291c1bd828f6995133d8ffa0d255
keith <devnull@localhost>
2007-12-18 04:47:04 +0700

    [svn r1106] rearrange finally so that all files are closed

--- src/org/thanlwinsoft/myanmar/MyanmarValidator.java
+++ src/org/thanlwinsoft/myanmar/MyanmarValidator.java
@@ -18,26 +18,27 @@ import java.util.logging.Logger;
 
 public class MyanmarValidator
 {
-    protected static Logger sLogger = Logger.getLogger(MyanmarValidator.class.getCanonicalName());
-    
+    protected static Logger sLogger = Logger.getLogger(MyanmarValidator.class
+            .getCanonicalName());
+
     public enum Status
     {
         Valid, Corrected, Invalid
     }
-    
+
     enum UTN11
     {
-        Unknown(0),
-        Consonant(1), Asat(2), Stacker(3), MedialYR(4),
-        MedialW(5), MedialH(6), EVowel(7), UVowel(8), 
-        LVowel(9), AVowel(10), Anusvara(11), VisibleVirama(12), 
-        LowerDot(13), Visarga(14);
-        
+        Unknown(0), Consonant(1), Asat(2), Stacker(3), MedialYR(4), MedialW(5), MedialH(
+                6), EVowel(7), UVowel(8), LVowel(9), AVowel(10), Anusvara(11), VisibleVirama(
+                12), LowerDot(13), Visarga(14);
+
         int mSequenceId;
+
         UTN11(int seq)
         {
             mSequenceId = seq;
         }
+
         static UTN11 fromCode(char c)
         {
             // deal with the ranges
@@ -76,31 +77,52 @@ public class MyanmarValidator
                 return LowerDot;
             case 0x1038:
                 return Visarga;
-            
+
             }
             return Unknown;
         }
-        public int getSequenceId() { return mSequenceId; }
+
+        public int getSequenceId()
+        {
+            return mSequenceId;
+        }
     }
-    
+
     private long mLine = 1;
     private long mColumn = 0;
     private long mTabWidth = 4;
     private static final String sEOL = System.getProperty("line.separator");
-    
+
     public MyanmarValidator()
     {
         sLogger.setLevel(Level.ALL);
     }
-    
-    public void setTabWidth(int w) { mTabWidth = w; }
-    public void reset() { mLine = 1; mColumn = 0; }
-    public long getLineNumber() { return mLine; };
-    public long getColumn() { return mColumn; }
-    
+
+    public void setTabWidth(int w)
+    {
+        mTabWidth = w;
+    }
+
+    public void reset()
+    {
+        mLine = 1;
+        mColumn = 0;
+    }
+
+    public long getLineNumber()
+    {
+        return mLine;
+    };
+
+    public long getColumn()
+    {
+        return mColumn;
+    }
+
     public Status validate(BufferedReader r, BufferedWriter w)
     {
-        Deque <Character> utn11Queue = new ArrayDeque <Character>(UTN11.Visarga.getSequenceId());
+        Deque<Character> utn11Queue = new ArrayDeque<Character>(UTN11.Visarga
+                .getSequenceId());
         Status valid = Status.Valid;
         try
         {
@@ -109,15 +131,17 @@ public class MyanmarValidator
                 try
                 {
                     int utf32 = r.read();
-		    if (utf32 < 0) break; // end 
-                    char [] utf16 = Character.toChars(utf32);
-                    if (utf16[0] == (char)'\t') 
+                    if (utf32 < 0)
+                        break; // end
+                    char[] utf16 = Character.toChars(utf32);
+                    if (utf16[0] == (char) '\t')
                     {
                         mColumn += mTabWidth;
                         long overshoot = mColumn % mTabWidth;
                         mColumn -= overshoot;
                     }
-                    else mColumn+= utf16.length;
+                    else
+                        mColumn += utf16.length;
 
                     if (utf16.length > 1)
                     {
@@ -140,7 +164,7 @@ public class MyanmarValidator
                             w.write(utf16);
                             continue;
                         }
-                        // if there is no stack, just add it and loop, normally 
+                        // if there is no stack, just add it and loop, normally
                         // this will only occur for consonants
                         if (utn11Queue.size() == 0)
                         {
@@ -148,10 +172,12 @@ public class MyanmarValidator
                             continue;
                         }
                         UTN11 prevSeq = UTN11.fromCode(utn11Queue.peek());
-                        // 0x103A needs special handling, since it occurs twice in the sequence
+                        // 0x103A needs special handling, since it occurs twice
+                        // in the sequence
                         if (seq == UTN11.Asat && prevSeq != UTN11.Consonant)
                             seq = UTN11.VisibleVirama;
-                        // Is it the next in the sequence within the syllable structure?
+                        // Is it the next in the sequence within the syllable
+                        // structure?
                         if (prevSeq.getSequenceId() < seq.getSequenceId())
                         {
                             utn11Queue.push(utf16[0]);
@@ -166,23 +192,24 @@ public class MyanmarValidator
                             continue;
                         }
                         // something has probably gone wrong
-                        
+
                         // Check for some common mistakes and fix them
                         if (prevSeq == UTN11.LVowel && seq == UTN11.UVowel)
                         {
                             char lv = utn11Queue.pop();
                             utn11Queue.push(utf16[0]);
                             utn11Queue.push(lv);
-                            if (valid == Status.Valid) valid = Status.Corrected;
-                            sLogger.fine("Corrected at: Ln " + mLine + " Col " + 
-                                    (mColumn - utn11Queue.size()) +
-                                    "," + mColumn + " " + dumpQueue(utn11Queue));
+                            if (valid == Status.Valid)
+                                valid = Status.Corrected;
+                            sLogger.fine("Corrected at: Ln " + mLine + " Col "
+                                    + (mColumn - utn11Queue.size()) + ","
+                                    + mColumn + " " + dumpQueue(utn11Queue));
                             continue;
                         }
                         utn11Queue.push(utf16[0]);
-                        sLogger.warning("Invalid sequence at: Ln " + mLine + " Col " + 
-                                (mColumn - utn11Queue.size()) +
-                                "," + mColumn + " " + dumpQueue(utn11Queue));
+                        sLogger.warning("Invalid sequence at: Ln " + mLine
+                                + " Col " + (mColumn - utn11Queue.size()) + ","
+                                + mColumn + " " + dumpQueue(utn11Queue));
                         valid = Status.Invalid;
                     }
                 }
@@ -190,27 +217,29 @@ public class MyanmarValidator
                 {
                     break;
                 }
-            } while(true);
+            } while (true);
         }
-        //catch (IOException e)
-	finally
+        // catch (IOException e)
+        finally
         {
-            
+
         }
         return valid;
     }
-    
-    private Status writeQueue(Deque <Character> utn11Queue, BufferedWriter w, Status status) throws IOException
+
+    private Status writeQueue(Deque<Character> utn11Queue, BufferedWriter w,
+            Status status) throws IOException
     {
         if (utn11Queue.size() > 0 && utn11Queue.peek() == 0x1039)
         {
             // stray 1039, should probably be 103A
             utn11Queue.pop();
-            utn11Queue.push(Character.valueOf((char)0x103A));
-            sLogger.fine("Corrected 0x1039 at: Ln " + mLine + " Col " + 
-                    (mColumn - utn11Queue.size()) +
-                    "," + mColumn + " " + dumpQueue(utn11Queue));
-            if (status == Status.Valid) status = Status.Corrected;
+            utn11Queue.push(Character.valueOf((char) 0x103A));
+            sLogger.fine("Corrected 0x1039 at: Ln " + mLine + " Col "
+                    + (mColumn - utn11Queue.size()) + "," + mColumn + " "
+                    + dumpQueue(utn11Queue));
+            if (status == Status.Valid)
+                status = Status.Corrected;
         }
         // write the characters, starting with the oldest char on the stack
         while (utn11Queue.size() > 0)
@@ -219,17 +248,18 @@ public class MyanmarValidator
         }
         return status;
     }
-    
-    private final String dumpQueue(Deque <Character> utn11Queue) 
+
+    private final String dumpQueue(Deque<Character> utn11Queue)
     {
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
-        Iterator <Character> i = utn11Queue.iterator();
+        Iterator<Character> i = utn11Queue.iterator();
         while (i.hasNext())
         {
             char c = i.next();
             // iterating backwards from right to left
-            if (sb2.length() > 0) sb2.insert(0, " ");
+            if (sb2.length() > 0)
+                sb2.insert(0, " ");
             sb2.insert(0, Integer.toHexString(c));
             sb2.insert(0, "0x");
             sb1.insert(0, c);
@@ -238,7 +268,7 @@ public class MyanmarValidator
         return sb2.toString();
     }
 
-    public static void main(String [] arg)
+    public static void main(String[] arg)
     {
         if (arg.length < 2)
         {
@@ -248,38 +278,41 @@ public class MyanmarValidator
         }
         BufferedReader br = null;
         BufferedWriter bw = null;
-        try
+
+        File outputDir = new File(arg[arg.length - 1]);
+        if (!outputDir.isDirectory())
         {
-            File outputDir = new File(arg[arg.length - 1]);
-            if (!outputDir.isDirectory())
+            if (arg.length > 2)
             {
-                if (arg.length > 2)
-                {
-                    System.err.println("Last argument should be a directory");
-                    System.exit(2);
-                }
-                outputDir = null;
+                System.err.println("Last argument should be a directory");
+                System.exit(2);
             }
-            for (int i = 0; i < arg.length - 1; i++)
+            outputDir = null;
+        }
+        for (int i = 0; i < arg.length - 1; i++)
+        {
+            File inputFile = new File(arg[i]);
+            File outputFile = null;
+            try
             {
-                File inputFile = new File(arg[i]);
-                File outputFile = null;
-                br = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), "UTF-8"));
+                br = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(inputFile), "UTF-8"));
                 if (outputDir != null)
                 {
                     outputFile = new File(outputDir, inputFile.getName());
                 }
                 else
                 {
-                    assert(arg.length == 2);
+                    assert (arg.length == 2);
                     outputFile = new File(arg[arg.length - 1]);
                 }
                 if (inputFile.equals(outputFile))
                 {
-                    System.err.println("input and output can't be the same file!");
+                    System.err.println("input/output can't be the same file!");
                     System.exit(3);
                 }
-                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8"));
+                bw = new BufferedWriter(new OutputStreamWriter(
+                        new FileOutputStream(outputFile), "UTF-8"));
 
                 MyanmarValidator validator = new MyanmarValidator();
                 if (System.getProperty("os.name").equals("Linux"))
@@ -288,36 +321,34 @@ public class MyanmarValidator
                 if (status != Status.Valid)
                     sLogger.info(arg[i] + " Validation state: " + status);
             }
-        }
-        catch (FileNotFoundException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        finally 
-        {
-            try
+            catch (FileNotFoundException e)
             {
-                if (br != null) br.close();
+                sLogger.log(Level.SEVERE, e.getMessage(), e);
             }
-            catch (IOException e)
+            catch (UnsupportedEncodingException e)
             {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                sLogger.log(Level.SEVERE, e.getMessage(), e);
             }
-            try
+            finally
             {
-                if (bw != null) bw.close();
-            }
-            catch (IOException e)
-            {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                try
+                {
+                    if (br != null)
+                        br.close();
+                }
+                catch (IOException e)
+                {
+                    sLogger.log(Level.SEVERE, e.getMessage(), e);
+                }
+                try
+                {
+                    if (bw != null)
+                        bw.close();
+                }
+                catch (IOException e)
+                {
+                    sLogger.log(Level.SEVERE, e.getMessage(), e);
+                }
             }
         }
         System.exit(0);
